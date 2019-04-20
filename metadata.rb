96997e8d91ab4522226770c758b9cf1339104a45
hieunba <hi3unb@gmail.com>
2019-04-20 10:15:11 +0700

    Initial release

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,22 @@
 name 'qt'
-maintainer 'The Authors'
-maintainer_email 'hnghiemba@me.com'
+maintainer 'Nghiem Ba Hieu'
+maintainer_email 'hnghiemba@pm.me'
 license 'Apache-2.0'
 description 'Installs/Configures qt'
 long_description 'Installs/Configures qt'
 version '0.1.0'
 chef_version '>= 13.0'
 
+supports 'windows'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/qt/issues'
+issues_url 'https://github.com/hieunba/chef_qt/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/qt'
+source_url 'https://github.com/hieunba/chef_qt'
